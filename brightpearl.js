10221846d7f6e8828d7f299a0f132f85a0b7aa0d
Joe Prisk <kernowjoe@gmail.com>
2018-02-25 22:37:17 +0000

    brightpearl initial export convert products into brightpearl import csv images not yet attached full description, specifications, features all contain html

--- brightpearl.js
+++ brightpearl.js
@@ -1,7 +1,7 @@
 const csvToJson        = require('csvtojson'),
 	  helpers          = require('./lib/helpers');
 
-let products = {},
+let products = [],
 	promises = [];
 
 csvToJson()
@@ -9,27 +9,29 @@ csvToJson()
 	.on(
 		'json',
 		product => promises.push(
-			Promise
-				.resolve()
-				.then(() => helpers.formatBlankLines(product))
-				.then(() => helpers.isFullProduct(product))
-				.then(() => helpers.editHeaderTags(product))
-				.then(() => helpers.addDescriptionHeader(product))
-				.then(() => helpers.removeEmptyLines(product))
-				.then(() => helpers.removeEmptyHeaders(product))
-				.then(() => addToProducts(product))
-				.catch(() => addToProducts(product))
-		)
+                Promise
+                    .resolve()
+                    .then(() => helpers.formatBlankLines(product))
+                    .then(() => helpers.isFullProduct(product))
+                    .then(() => helpers.extractDescription(product))
+                    .then((product) => addToProducts(product))
+                    .catch(()  => {})
+                    // addToProducts(product))
+            )
 	)
 	.on(
 		'done',
 		(error) => Promise
 			.all(promises)
-			.then(() => helpers.saveProductToCsv(error, helpers.PATHS.save, products))
+			.then(() => helpers.saveProductBrightPearl(error, helpers.PATHS.brightpearl, products))
 	);
 
 
 function addToProducts(product) {
+
+	products.push(product);
+
+	return Promise.resolve();
 
 	return Promise.resolve()
 				  .then(() => createEmptyProduct(product))
