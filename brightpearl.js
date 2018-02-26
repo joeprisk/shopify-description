a4254f7f49326a4e6bb1917983006acbbc292064
Joe Prisk <joe.prisk@headforwards.com>
2018-02-26 14:43:13 +0000

    brightpearl changes to include extra images in export

--- brightpearl.js
+++ brightpearl.js
@@ -14,9 +14,8 @@ csvToJson()
                     .then(() => helpers.formatBlankLines(product))
                     .then(() => helpers.isFullProduct(product))
                     .then(() => helpers.extractDescription(product))
-                    .then((product) => addToProducts(product))
-                    .catch(()  => {})
-                    // addToProducts(product))
+                    .then(() => addToProducts(product))
+                    .catch(()  => addToProducts(product))
             )
 	)
 	.on(
@@ -29,20 +28,16 @@ csvToJson()
 
 function addToProducts(product) {
 
-	products.push(product);
-
-	return Promise.resolve();
-
 	return Promise.resolve()
 				  .then(() => createEmptyProduct(product))
 				  .then(() => helpers.isFullProduct(product))
-				  .then(() => products[product.Handle].unshift(product))
-				  .catch(() => products[product.Handle].push(product));
+				  .then(() => products[product.Handle].brightPearl = product.brightPearl)
+				  .catch(() => products[product.Handle].images.push(product));
 }
 
 function createEmptyProduct(product) {
 
-	!products[product.Handle] && Object.assign(products, {[product.Handle]: []});
+	!products[product.Handle] && Object.assign(products, {[product.Handle]: {images: []}});
 
 	return Promise.resolve();
 }
